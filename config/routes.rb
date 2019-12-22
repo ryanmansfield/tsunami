f71198902ae5de625a7b1d84a87fdb44e09448fe
Ryan Mansfield <ryanmansfield86@gmail.com>
2019-12-21 19:50:44 -0600

    Added routes for shows, bio, music, and booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'bio', to: 'pages#bio', as: :bio
+  get 'shows', to: 'pages#shows', as: :shows
+  get 'music', to: 'pages#music', as: :music
+  get 'booking', to: 'pages#booking', as: :booking
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
